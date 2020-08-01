1337901119452150767f6e5ea1a07206c4dec30a
zoujt <756967944@qq.com>
2020-08-01 23:37:53 +0800

    项目基本完成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './common/header'
+import Home from './pages/home'
+import Detail from './pages/detail'
+//import Login from './pages/login'
 import store from './store'
 
 function App() {
   return (
     <Provider store={store}>
-      <Header/>
+      <BrowserRouter>
+      		<div>
+            <Header />
+      			<Route path='/' exact component={Home}></Route>
+            {/* <Route path='/login' exact component={Login}></Route> */}
+      			<Route path='/detail/:id' exact component={Detail}></Route>
+      		</div>
+      	</BrowserRouter>
     </Provider>
   )
 }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import { toJS } from 'immutable'
 import { actionCreators } from './store'
+import { Link } from 'react-router-dom'
 import {
   HeaderWrapper,
   Logo,
@@ -57,7 +58,9 @@ class Header extends Component {
     const { focused, list, handleInputFocus, handleInputBlur } = this.props
     return (
       <HeaderWrapper>
-        <Logo/>
+        <Link to='/'>
+					<Logo/>
+				</Link>
         <Nav>
           <NavItem className='left active'>首页</NavItem>
           <NavItem className='left'>下载App</NavItem>
